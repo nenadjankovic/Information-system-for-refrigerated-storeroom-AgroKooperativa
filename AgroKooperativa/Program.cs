31e1d99b040774853945e6852e99ec0c820a239b
Nenad Janković <nenadjankovic617@gmail.com>
2022-09-17 02:23:59 +0200

    17/9/2022

--- AgroKooperativa/Program.cs
+++ AgroKooperativa/Program.cs
@@ -16,7 +16,7 @@ namespace AgroKooperativa
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Proizvodjaci());
         }
     }
 }
--- AgroKooperativa/Registracija.Designer.cs
+++ AgroKooperativa/Registracija.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registracija));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -43,82 +44,91 @@
             this.errPrezime = new System.Windows.Forms.ErrorProvider(this.components);
             this.errKorisnickoIme = new System.Windows.Forms.ErrorProvider(this.components);
             this.errLozinka = new System.Windows.Forms.ErrorProvider(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.errIme)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errPrezime)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errKorisnickoIme)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errLozinka)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 39);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(191, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ime:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 65);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(191, 59);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Prezime:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 90);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(191, 84);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.Size = new System.Drawing.Size(93, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Korisničko ime:";
             // 
             // lblLozinka
             // 
             this.lblLozinka.AutoSize = true;
-            this.lblLozinka.Location = new System.Drawing.Point(19, 116);
+            this.lblLozinka.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLozinka.Location = new System.Drawing.Point(191, 110);
             this.lblLozinka.Name = "lblLozinka";
-            this.lblLozinka.Size = new System.Drawing.Size(47, 13);
+            this.lblLozinka.Size = new System.Drawing.Size(55, 13);
             this.lblLozinka.TabIndex = 3;
             this.lblLozinka.Text = "Lozinka:";
             // 
             // btnRegistrujSe
             // 
-            this.btnRegistrujSe.Location = new System.Drawing.Point(84, 165);
+            this.btnRegistrujSe.Image = ((System.Drawing.Image)(resources.GetObject("btnRegistrujSe.Image")));
+            this.btnRegistrujSe.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistrujSe.Location = new System.Drawing.Point(249, 150);
             this.btnRegistrujSe.Name = "btnRegistrujSe";
-            this.btnRegistrujSe.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrujSe.Size = new System.Drawing.Size(93, 32);
             this.btnRegistrujSe.TabIndex = 4;
             this.btnRegistrujSe.Text = "Registruj se";
+            this.btnRegistrujSe.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnRegistrujSe.UseVisualStyleBackColor = true;
             this.btnRegistrujSe.Click += new System.EventHandler(this.btnRegistrujSe_Click);
             // 
             // txtIme
             // 
-            this.txtIme.Location = new System.Drawing.Point(103, 36);
+            this.txtIme.Location = new System.Drawing.Point(290, 30);
             this.txtIme.Name = "txtIme";
             this.txtIme.Size = new System.Drawing.Size(100, 20);
             this.txtIme.TabIndex = 5;
             // 
             // txtPrezime
             // 
-            this.txtPrezime.Location = new System.Drawing.Point(103, 62);
+            this.txtPrezime.Location = new System.Drawing.Point(290, 55);
             this.txtPrezime.Name = "txtPrezime";
             this.txtPrezime.Size = new System.Drawing.Size(100, 20);
             this.txtPrezime.TabIndex = 6;
             // 
             // txtKorisnicko
             // 
-            this.txtKorisnicko.Location = new System.Drawing.Point(103, 87);
+            this.txtKorisnicko.Location = new System.Drawing.Point(290, 81);
             this.txtKorisnicko.Name = "txtKorisnicko";
             this.txtKorisnicko.Size = new System.Drawing.Size(100, 20);
             this.txtKorisnicko.TabIndex = 7;
             // 
             // txtLozinka
             // 
-            this.txtLozinka.Location = new System.Drawing.Point(103, 113);
+            this.txtLozinka.Location = new System.Drawing.Point(290, 107);
             this.txtLozinka.Name = "txtLozinka";
             this.txtLozinka.PasswordChar = '*';
             this.txtLozinka.Size = new System.Drawing.Size(100, 20);
@@ -126,11 +136,14 @@
             // 
             // btnNazad
             // 
-            this.btnNazad.Location = new System.Drawing.Point(84, 194);
+            this.btnNazad.Image = ((System.Drawing.Image)(resources.GetObject("btnNazad.Image")));
+            this.btnNazad.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNazad.Location = new System.Drawing.Point(259, 188);
             this.btnNazad.Name = "btnNazad";
-            this.btnNazad.Size = new System.Drawing.Size(75, 23);
+            this.btnNazad.Size = new System.Drawing.Size(72, 29);
             this.btnNazad.TabIndex = 9;
             this.btnNazad.Text = "Nazad";
+            this.btnNazad.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnNazad.UseVisualStyleBackColor = true;
             this.btnNazad.Click += new System.EventHandler(this.btnNazad_Click);
             // 
@@ -150,11 +163,22 @@
             // 
             this.errLozinka.ContainerControl = this;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 210);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
             // Registracija
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(244, 229);
+            this.ClientSize = new System.Drawing.Size(413, 229);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnNazad);
             this.Controls.Add(this.txtLozinka);
             this.Controls.Add(this.txtKorisnicko);
@@ -165,6 +189,9 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Registracija";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Registracija";
@@ -173,6 +200,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.errPrezime)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.errKorisnickoIme)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.errLozinka)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,5 +222,6 @@
         private System.Windows.Forms.ErrorProvider errPrezime;
         private System.Windows.Forms.ErrorProvider errKorisnickoIme;
         private System.Windows.Forms.ErrorProvider errLozinka;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
